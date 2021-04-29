dec68704aa8efcac602b7eeec0880ce190e30b00
olssssi <64044694+olssssi@users.noreply.github.com>
2021-04-29 15:09:05 +0200

    Registration added
    Added subpage accounts/signup

--- mysite/dysk/urls.py
+++ mysite/dysk/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from django.contrib import admin
 
 from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
