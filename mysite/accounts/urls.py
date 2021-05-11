10804345589967fc0e5891be4842f4fddbf789c3
Jakub <burza.kuba@gmail.com>
2021-05-11 12:51:08 +0200

    Reset hasla przy uzyciu maila

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import SignUpView
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/reset_password.html"),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_sent.html"),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_form.html"),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"),
+         name='password_reset_complete')
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -149,3 +149,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # config/settings.py
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'projektdysku@gmail.com'
+EMAIL_HOST_PASSWORD = 'ZAQ!2wsx'
+EMAIL_USE_TLS = True
