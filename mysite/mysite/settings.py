2fdf4202e59e4f6a67f08ac0e667e01255847062
Jakub <burza.kuba@gmail.com>
2021-05-27 14:31:05 +0200

    nowa baza połączenie

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'zxxqqaif',
+#         'USER': 'zxxqqaif',
+#         'PASSWORD': 'Vp5TgzZtf7kuyBSPwEkTsbZYe6RMFO82',
+#         'HOST': 'tai.db.elephantsql.com',
+#         'PORT': '5432',
+#
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'zxxqqaif',
-        'USER': 'zxxqqaif',
-        'PASSWORD': 'Vp5TgzZtf7kuyBSPwEkTsbZYe6RMFO82',
-        'HOST': 'tai.db.elephantsql.com',
+        'NAME': 'rrqgmstv',
+        'USER': 'rrqgmstv',
+        'PASSWORD': 'GS1-pMG8sloaXRsUyMRIBCPbVe-Kum1R',
+        'HOST': 'manny.db.elephantsql.com',
         'PORT': '5432',
 
     }
